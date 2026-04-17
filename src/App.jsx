4584dd8f159bbe8e2c7a32c0b78ddcfb699c5736
richard kazuo takada leal <richardktleal@gmail.com>
2026-04-17 13:23:33 -0300

    Estrutura inicial com vite e bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="app-fundo">
+      <div className="container py-4">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-10">
+            <div className="app-card p-4">
+              <h1 className="titulo text-center mb-4">Hello, Investimentos</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
